685a1b0e2b33d895c4baeb38af0b86ec9f5f731a
Stormie Renevey <sarenevey@icloud.com>
2024-04-09 23:25:54 -0500

    readme.txt

--- backend/zones/admin.py
+++ backend/zones/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Property, Zone
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Property)
+admin.site.register(Zone)
